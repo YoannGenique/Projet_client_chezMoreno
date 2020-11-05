477408df82faa9e4600b45c0c087b220b557a18c
Yoann Génique <72612379+YoannGenique@users.noreply.github.com>
2020-11-05 13:04:58 +0100

    Ajout de deux des 4 pages du site.
    La Page Profil, et l'index/formulaire de co
    Fait par Célia Rameau

---
+++ profil.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Espacepersonelle</title>
+    <link rel="stylesheet" href="test.css">
+</head>
+<body>
+    
+    <header>
+        <div class="logo">
+            <img src="media/logo.png" alt="logo">
+        </div>
+        <h1> Chez moreno ! Attendez vous à une veranda en béton ! </h1>
+    </header>
+   <div>
+        <img src="media/carreaux.jpg" width="865px" height="865px" alt="motifacarreaux">
+    </div>    
+</body>
+</html>
